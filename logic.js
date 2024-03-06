efd31acd056398109e7f2ab4fa3ed1e588184dcc
DiegoO2 <diego.hidalgo23@student.cart.org>
2024-03-06 14:50:55 -0800

    An
    Finish the css and starting the javascript

--- logic.js
+++ logic.js
@@ -3,5 +3,20 @@ document.addEventListener(`DOMContentLoaded`, () => {
     let elementById = function(id) {return document.getElementById(id)}
 
     let addListener = function(id,event,code) {elementById(id).addEventListener(event, (eventData) => {code(eventData)})}
+    let step = 1;
 
+    addListener("nextButton", "change",
+
+    switch(step){
+        case 1:
+
+        case 2:
+
+        case 3:
+
+        case 4:
+    }
+    
+    
+    )
 })
